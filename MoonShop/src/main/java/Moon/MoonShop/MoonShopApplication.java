4b4eebe300f1fc535452ceba088c47e420a11d49
MoonHyunSeong <gustjd617@gmail.com>
2022-08-03 12:21:50 +0900

    make project
    - 프로젝트 생성 완료
    - 스마트스토어 느낌의 프로젝트 구현 예정

---
+++ MoonShop/src/main/java/Moon/MoonShop/MoonShopApplication.java
@@ -0,0 +1,13 @@
+package Moon.MoonShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonShopApplication.class, args);
+	}
+
+}
---
+++ MoonShop/src/test/java/Moon/MoonShop/MoonShopApplicationTests.java
@@ -0,0 +1,13 @@
+package Moon.MoonShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
